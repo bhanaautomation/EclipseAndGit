cf8adbb7542b3e510baf27ca344a6106756f3ce8
bhanaautomation <bhanaautomation@gmail.com>
2021-08-17 11:13:47 -0400

    Added new Plugins in Maven

--- src/main/java/testNG/Maven/Hope.java
+++ src/main/java/testNG/Maven/Hope.java
@@ -7,5 +7,15 @@ public class Hope {
  	public static void HopeW() {
  		System.out.println("How this will Work");
  		System.out.println("How this will Work");
- 	} 
+ 	}
+	@Test ()
+ 	public static void HopeW2() {
+ 		System.out.println("How this will Work2");
+ 		System.out.println("How this will Work2");
+ 	}
+	@Test ()
+ 	public static void HopeW3() {
+ 		System.out.println("How this will Work3");
+ 		System.out.println("How this will Work3");
+ 	}
 }
